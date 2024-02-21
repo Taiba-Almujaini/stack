16c1dfbbefe0fe8d6f636aca242d797505a23be3
Taiba-Almujaini <almujaini1995.t@gmail.com>
2024-02-21 12:19:48 +0400

    Stack Initialization

---
+++ Stack Initialization.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/*Problem Statement: Write a program to initialize an empty stack. Ensure that your stack can hold up to 10 integers.
+
+        Objective: Familiarize yourself with the basic structure of a stack..*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.setSize(10); // stack can hold up  10 integers only.
+
+
+
+
+        }
+    }
